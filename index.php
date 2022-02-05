5611d2d28d25a668a221a935dc6e4bba203cb322
KHELIFA Yassine <prostam27@gmail.com>
2022-02-05 21:10:24 +0100

    install Symf../routing

--- index.php
+++ index.php
@@ -1,56 +1,68 @@
 <?php
 
-/**
- * BIENVENUE DANS CE COURS SUR LE COMPOSANT SYMFONY/ROUTING !
- * ----------------------
- * Dans ce cours nous allons étudier ce fabuleux composant qui permet de créer et de gérer des routes (adresses) intelligentes et intelligibles !
- * 
- * PRESENTATION DE L'APPLICATION (SIMPLE) :
- * ----------------
- * Nous avons ici une application de gestion de tâches très simple : pas de base de données, pas de réelles manipulations de données, ce n'est
- * qu'à des fins d'exemples.
- * 
- * Elle possède 3 pages distinctes :
- * - /index.php (ou /index.php?page=list) : permet d'afficher la liste des tâches contenue dans le fichier data.php (voir le fichier pages/list.php)
- * - /index.php?page=show&id=100 : permet d'afficher la tâche dont l'identifiant est 100 en détails (voir le fichier pages/show.php)
- * - /index.php?page=create (en GET) : permet d'afficher le formulaire de création (voir le fichier pages/create.php)
- * - /index.php?page=create (en POST) : permet de traiter le formulaire de création (toujours dans pages/create.php)
- * 
- * CREER DES ROUTES PERSONNALISEES (ET JOLIES ?) :
- * ----------------
- * On souhaite désormais pouvoir gérer tout ça avec des routes plus "propres" :
- * - /index.php (ou /index.php?page=list) deviendrait juste / 
- * - /index.php?page=show&id=100 deviendrait /show/100
- * - /index.php?page=create (en GET) deviendrait /create (en GET)
- * - /index.php?page=create (en POST) deviendrait /create (en POST)
- * 
- * Ca vous dit ? Alors commencez par bien analayser l'application dans son état actuel pour bien la comprendre, et passez à la section suivante
- * 
- */
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+
+require __DIR__ . "/vendor/autoload.php";
+
+
+$pathInfo = $_SERVER['PATH_INFO'] ?? '/';
+
+$listRoute   = new Route("/");
+$createRoute = new Route("/create");
+$showRoute   = new Route("/show");
+
+$collection = new RouteCollection();
+$collection->add('list', $listRoute);
+$collection->add('create', $createRoute);
+$collection->add('show', $showRoute);
+
+
+$matcher = new UrlMatcher($collection, new RequestContext());
+
+try{
+    $res = $matcher->match( $pathInfo );
+
+    require_once "pages/".$res["_route"].".php";
+
+
+}catch(ResourceNotFoundException $e){
+    require 'pages/404.php';
+    return;
+}
+
+// var_dump($res);
+// die();
+
+
+
 
 /**
  * LES PAGES DISPONIBLES
  * ---------
  * Afin de pouvoir être sur que le visiteur souhaite voir une page existante, on maintient ici une liste des pages existantes
  */
-$availablePages =  [
-    'list', 'show', 'create'
-];
+// $availablePages =  [
+//     'list', 'show', 'create'
+// ];
 
 // Par défaut, la page qu'on voudra voir si on ne précise pas (par exemple sur /index.php) sera "list"
-$page = 'list';
+//$page = 'list';
 
 // Si on nous envoi une page en GET, on la prend en compte (exemple : /index.php?page=create)
-if (isset($_GET['page'])) {
-    $page = $_GET['page'];
-}
+// if (isset($_GET['page'])) {
+//     $page = $_GET['page'];
+// }
 
 // Si la page demandée n'existe pas (n'est pas dans le tableau $availablePages)
 // On affiche la page 404
-if (!in_array($page, $availablePages)) {
-    require 'pages/404.php';
-    return;
-}
+// if (!in_array($page, $availablePages)) {
+//     require 'pages/404.php';
+//     return;
+// }
 
 /**
  * ❌ ATTENTION DEMANDEE !
@@ -71,4 +83,3 @@ if (!in_array($page, $availablePages)) {
  * La conséquence, c'est que si demain je décide que le formulaire de création devrait se trouver sur /index.php?page=new il faudra que je
  * renomme forcément le fichier pages/create.php en pages/new.php et inversement (l'enfer)
  */
-require_once "pages/$page.php";
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '2e14110e8930a012d9ed7b39194838d568ca9b27',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '2e14110e8930a012d9ed7b39194838d568ca9b27',
+            'dev_requirement' => false,
+        ),
+        'symfony/routing' => array(
+            'pretty_version' => 'v6.0.3',
+            'version' => '6.0.3.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symfony/routing',
+            'aliases' => array(),
+            'reference' => 'b1debdf7a40e6bc7eee0f363ab9dd667fe04f099',
+            'dev_requirement' => false,
+        ),
+    ),
+);
